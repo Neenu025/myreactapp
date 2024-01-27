1d3f39434c0c94a9a425349505e9b0c77d08ece7
Neenu025 <neenujose025@gmail.com>
2024-01-27 16:07:07 +0530

    React initial steps

---
+++ src/components/Child.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child() {
+  return (
+    <div>
+        <h2>Child of Home component</h2>
+    </div>
+  )
+}
+
+export default Child
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+
+const Header = () => {
+    
+  return (
+    <div>
+        <h1 className='header'>Component1</h1>
+        <p className='para'>This is a test for css functioning</p>
+    </div>
+  )
+}
+
+export default Header
